a4644b9fbc0a8a4cda0f6afd30166de0c8d93428
JoãoVitordC <sojohhny@gmail.com>
2021-01-29 04:26:11 -0300

    Java List exercises focused on OO

---
+++ Listas00/src/entities/Employee.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Employee{
+    private Integer Id;
+    private String Name;
+    private Double Salary;
+
+    public Employee(Integer id, String name, Double salary) {
+        Id = id;
+        Name = name;
+        Salary = salary;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Double salary) {
+        Salary = salary;
+    }
+    public void increaseSalary(double percentege){
+        Salary *= Salary * percentege / 100.0;
+    }
+
+}
